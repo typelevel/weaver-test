56dbc8789aadf67616db696ac47bfe1fd9ba8ac0
zainab-ali <zainab@kebab-ca.se>
2024-04-03 14:54:02 +0100

    Add Comparison trait.

---
+++ modules/core/shared/src/main/scala/weaver/Comparison.scala
@@ -0,0 +1,34 @@
+package weaver
+
+import cats.Eq
+import cats.Show
+import com.eed3si9n.expecty._
+
+trait Comparison[A] {
+  def diff(expected: A, found: A): Option[String]
+}
+
+object Comparison {
+  def fromEqAndShow[A](
+      implicit eqv: Eq[A] = Eq.fromUniversalEquals[A],
+      showA: Show[A] = Show.fromToString[A]
+  ): Comparison[A] = {
+    new Comparison[A] {
+      def diff(expected: A, found: A): Option[String] = {
+        if (eqv.eqv(found, expected)) {
+          None
+        } else {
+          val expectedLines = showA.show(expected).linesIterator.toSeq
+          val foundLines    = showA.show(found).linesIterator.toSeq
+          val diff = DiffUtil
+            .mkColoredLineDiff(expectedLines, foundLines)
+            .linesIterator
+            .toSeq
+            .map(str => Console.RESET.toString + str)
+            .mkString("\n")
+          Some(diff)
+        }
+      }
+    }
+  }
+}
--- modules/core/shared/src/main/scala/weaver/internals/ExpectSame.scala
+++ modules/core/shared/src/main/scala/weaver/internals/ExpectSame.scala
@@ -5,8 +5,6 @@ import cats.Show
 import cats.data.{ NonEmptyList, Validated }
 import cats.kernel.Eq
 
-import com.eed3si9n.expecty._
-
 private[weaver] trait ExpectSame {
 
   def eql[A](
@@ -15,26 +13,8 @@ private[weaver] trait ExpectSame {
       implicit eqA: Eq[A],
       showA: Show[A] = Show.fromToString[A],
       loc: SourceLocation): Expectations = {
-
-    if (eqA.eqv(expected, found))
-      Expectations(Validated.validNel(()))
-    else {
-      val header = "Values not equal:"
-
-      val expectedLines = showA.show(expected).linesIterator.toSeq
-      val foundLines    = showA.show(found).linesIterator.toSeq
-      val sourceLocs    = NonEmptyList.of(loc)
-      val diff = DiffUtil
-        .mkColoredLineDiff(expectedLines, foundLines)
-        .linesIterator
-        .toSeq
-        .map(str => Console.RESET.toString + str)
-        .mkString("\n")
-
-      Expectations(
-        Validated.invalidNel[AssertionException, Unit](
-          new AssertionException(header + "\n\n" + diff, sourceLocs)))
-    }
+    val comparison = Comparison.fromEqAndShow(eqA, showA)
+    diff(expected, found)(comparison, loc)
   }
 
   /**
@@ -45,5 +25,21 @@ private[weaver] trait ExpectSame {
       found: A)(
       implicit eqA: Eq[A] = Eq.fromUniversalEquals[A],
       showA: Show[A] = Show.fromToString[A],
-      loc: SourceLocation): Expectations = eql(expected, found)
+      loc: SourceLocation): Expectations = eql(expected, found)(eqA, showA, loc)
+
+  def diff[A](
+      expected: A,
+      found: A)(
+      implicit comparisonA: Comparison[A],
+      loc: SourceLocation): Expectations = {
+    comparisonA.diff(expected, found) match {
+      case None => Expectations(Validated.validNel(()))
+      case Some(diff) =>
+        val header     = "Values not equal:"
+        val sourceLocs = NonEmptyList.of(loc)
+        Expectations(
+          Validated.invalidNel[AssertionException, Unit](
+            new AssertionException(header + "\n\n" + diff, sourceLocs)))
+    }
+  }
 }
