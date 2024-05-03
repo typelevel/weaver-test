664fc291bd1850994b081130d75769982b734d2d
zainab-ali <zainab@kebab-ca.se>
2024-05-03 10:31:14 +0100

    Update CheckConfig to a binary compatible case class.

--- modules/scalacheck/shared/src/main/scala/weaver/scalacheck/CheckConfig.scala
+++ modules/scalacheck/shared/src/main/scala/weaver/scalacheck/CheckConfig.scala
@@ -3,104 +3,37 @@ package scalacheck
 
 import org.scalacheck.rng.Seed
 
-final class CheckConfig(
+case class CheckConfig private (
     val minimumSuccessful: Int,
     val maximumDiscardRatio: Int,
     val maximumGeneratorSize: Int,
     val perPropertyParallelism: Int,
-    val initialSeed: Option[Seed]) extends Product with Serializable {
+    val initialSeed: Option[Seed]) {
   assert(maximumDiscardRatio >= 0)
   assert(maximumDiscardRatio <= 100)
   assert(minimumSuccessful > 0)
 
   def maximumDiscarded = minimumSuccessful * maximumDiscardRatio / 100
 
-  def withMinimumSuccessful(minimumSuccessful: Int) = new CheckConfig(
-    minimumSuccessful = minimumSuccessful,
-    maximumDiscardRatio = this.maximumDiscardRatio,
-    maximumGeneratorSize = this.maximumGeneratorSize,
-    perPropertyParallelism = this.perPropertyParallelism,
-    initialSeed = this.initialSeed
+  def withMinimumSuccessful(minimumSuccessful: Int) = copy(
+    minimumSuccessful = minimumSuccessful
   )
 
-  def withMaximumDiscardRatio(maximumDiscardRatio: Int) = new CheckConfig(
-    minimumSuccessful = this.minimumSuccessful,
-    maximumDiscardRatio = maximumDiscardRatio,
-    maximumGeneratorSize = this.maximumGeneratorSize,
-    perPropertyParallelism = this.perPropertyParallelism,
-    initialSeed = this.initialSeed
+  def withMaximumDiscardRatio(maximumDiscardRatio: Int) = copy(
+    maximumDiscardRatio = maximumDiscardRatio
   )
 
-  def withMaximumGeneratorSize(maximumGeneratorSize: Int) = new CheckConfig(
-    minimumSuccessful = this.minimumSuccessful,
-    maximumDiscardRatio = this.maximumDiscardRatio,
-    maximumGeneratorSize = maximumGeneratorSize,
-    perPropertyParallelism = this.perPropertyParallelism,
-    initialSeed = this.initialSeed
+  def withMaximumGeneratorSize(maximumGeneratorSize: Int) = copy(
+    maximumGeneratorSize = maximumGeneratorSize
   )
 
-  def withPerPropertyParallelism(perPropertyParallelism: Int) = new CheckConfig(
-    minimumSuccessful = this.minimumSuccessful,
-    maximumDiscardRatio = this.maximumDiscardRatio,
-    maximumGeneratorSize = this.maximumGeneratorSize,
-    perPropertyParallelism = perPropertyParallelism,
-    initialSeed = this.initialSeed
+  def withPerPropertyParallelism(perPropertyParallelism: Int) = copy(
+    perPropertyParallelism = perPropertyParallelism
   )
 
-  def withInitialSeed(initialSeed: Option[Seed]) = new CheckConfig(
-    minimumSuccessful = this.minimumSuccessful,
-    maximumDiscardRatio = this.maximumDiscardRatio,
-    maximumGeneratorSize = this.maximumGeneratorSize,
-    perPropertyParallelism = this.perPropertyParallelism,
+  def withInitialSeed(initialSeed: Option[Seed]) = copy(
     initialSeed = initialSeed
   )
-
-  override def toString: String = {
-    val b = new StringBuilder("CheckConfig(")
-    b.append(String.valueOf(this.minimumSuccessful))
-    b.append(", ")
-    b.append(String.valueOf(this.maximumDiscardRatio))
-    b.append(", ")
-    b.append(String.valueOf(this.maximumGeneratorSize))
-    b.append(", ")
-    b.append(String.valueOf(this.perPropertyParallelism))
-    b.append(", ")
-    b.append(String.valueOf(this.initialSeed))
-    b.append(")")
-    b.toString()
-  }
-
-  override def canEqual(obj: Any): Boolean =
-    obj != null && obj.isInstanceOf[CheckConfig]
-
-  override def equals(obj: Any): Boolean = canEqual(obj) && {
-    val other = obj.asInstanceOf[CheckConfig]
-    this.minimumSuccessful == other.minimumSuccessful && this.maximumDiscardRatio == other.maximumDiscardRatio && this.maximumGeneratorSize == other.maximumGeneratorSize &&
-    this.perPropertyParallelism == other.perPropertyParallelism && this.initialSeed == other.initialSeed
-  }
-
-  override def hashCode: Int = {
-    var code = 17 + "CheckConfig".##
-    code = 37 * code + this.minimumSuccessful.##
-    code = 37 * code + this.maximumDiscardRatio.##
-    code = 37 * code + this.maximumGeneratorSize.##
-    code = 37 * code + this.perPropertyParallelism.##
-    code = 37 * code + this.initialSeed.##
-    37 * code
-  }
-
-  override def productPrefix: String = "CheckConfig"
-
-  override def productArity: Int = 5
-
-  override def productElement(n: Int): Any = n match {
-    case 0 => this.minimumSuccessful
-    case 1 => this.maximumDiscardRatio
-    case 2 => this.maximumGeneratorSize
-    case 3 => this.perPropertyParallelism
-    case 4 => this.initialSeed
-    case n => throw new IndexOutOfBoundsException(n.toString)
-  }
 }
 
 object CheckConfig {
