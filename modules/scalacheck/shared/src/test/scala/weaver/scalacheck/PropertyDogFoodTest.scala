b7cc5259added0f47ddfe8c9d92a23e2a8de3bee
zainab-ali <zainab@kebab-ca.se>
2024-11-07 17:50:55 +0000

    Add failing test for discard ratio.

--- modules/scalacheck/shared/src/test/scala/weaver/scalacheck/PropertyDogFoodTest.scala
+++ modules/scalacheck/shared/src/test/scala/weaver/scalacheck/PropertyDogFoodTest.scala
@@ -73,6 +73,10 @@ object PropertyDogFoodTest extends IOSuite {
       s"Discarded more inputs (${Meta.DiscardedChecks.checkConfig.maximumDiscarded}) than allowed",
       dogfood.runSuite(Meta.DiscardedChecks))
   }
+  test("Discard ratio of zero should still run tests") {
+    dogfood =>
+      expectNoErrorMessage(dogfood.runSuite(Meta.NoDiscardsChecks))
+  }
 
   def expectErrorMessage(
       msg: String,
@@ -84,6 +88,14 @@ object PropertyDogFoodTest extends IOSuite {
       exists(errorLogs) { log =>
         expect(log.contains(msg))
       }
+    }
+
+  def expectNoErrorMessage(state: IO[DogFood.State]): IO[Expectations] =
+    state.map { case (logs, _) =>
+      val errorLogs = logs.collect {
+        case LoggedEvent.Error(msg) => msg
+      }
+      expect(errorLogs.size == 0)
     }
 }
 
@@ -127,6 +139,15 @@ object Meta {
     }
   }
 
+  trait SucceededChecks extends MetaSuite {
+
+    test("foobar") {
+      partiallyAppliedForall { (_: Int) =>
+        IO(expect(true))
+      }
+    }
+  }
+
   trait DiscardedChecks extends MetaSuite {
 
     test("Discards all the time") {
@@ -156,4 +177,19 @@ object Meta {
           1
         ) // to avoid overcounting of discarded checks
   }
+
+  object NoDiscardsChecks extends SucceededChecks {
+
+    override def partiallyAppliedForall: PartiallyAppliedForall = forall
+
+    override def checkConfig =
+      super.checkConfig
+        .withMinimumSuccessful(1)
+        // Set the discard ratio to 0. No discarded tests are permitted.
+        .withMaximumDiscardRatio(0)
+        .withPerPropertyParallelism(
+          1
+        ) // to avoid overcounting of discarded checks
+  }
+
 }
