4f1aedf31d5a3401838e63d0189a7c8b95839d3c
zainab-ali <zainab@kebab-ca.se>
2025-11-10 15:34:07 +0000

    Modify failFast to raise multiple expectation failures.

--- modules/core/shared/src/main/scala/weaver/exceptions.scala
+++ modules/core/shared/src/main/scala/weaver/exceptions.scala
@@ -8,8 +8,7 @@ sealed abstract class WeaverTestException private[weaver] (
 
 final class ExpectationFailed(
     private[weaver] val message: String,
-    private[weaver] val locations: NonEmptyList[SourceLocation])
-    extends WeaverTestException(message) {
+    private[weaver] val locations: NonEmptyList[SourceLocation]) {
   private[weaver] def withLocation(
       location: SourceLocation): ExpectationFailed =
     new ExpectationFailed(message, locations.append(location))
@@ -19,3 +18,8 @@ final class IgnoredException private[weaver] (
     private[weaver] val reason: String,
     private[weaver] val location: SourceLocation)
     extends WeaverTestException(reason)
+
+final class ExpectationsFailed(
+    private[weaver] val failures: NonEmptyList[ExpectationFailed])
+    extends WeaverTestException(
+      s"One or more expecations failed:\n${failures.map(_.message).toList.mkString(System.lineSeparator)}")
